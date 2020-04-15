841ca29eb2c4322f60d64f824ed1e86a4fc05a9d
MightyBoosh <david.brooke14@icloud.com>
2020-04-15 07:35:31 +0100

    1

--- hello_args.py
+++ hello_args.py
@@ -1,10 +1,59 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, request, render_template
+
 app = Flask(__name__)
 
 
 @app.route('/hello/<name>')
 def hello_name(name):
     return 'Hello %s!' % name
+
+@app.route('/blog/<int:postID>')
+def show_blog(postID):
+   return 'Blog Number %d' % postID
+
+@app.route('/rev/<float:revNo>')
+def revision(revNo):
+   return 'Revision Number %f' % revNo
+
+@app.route('/admin')
+def hello_admin():
+   return 'Hello Admin'
+
+@app.route('/guest/<guest>')
+def hello_guest(guest):
+   return 'Hello %s as Guest' % guest
+
+@app.route('/user/<name>')
+def hello_user(name):
+   if name =='admin':
+      return redirect(url_for('hello_admin'))
+   else:
+      return redirect(url_for('hello_guest',guest = name))
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+
+@app.route('/hello/<int:score>')
+def hello_name1(score):
+   return render_template('hello.html', marks = score)
+
+
+@app.route('/results')
+def result():
+   dict = {'phy':50,'che':60,'maths':70}
+   return render_template('results.html', result = dict)
 
 
 if __name__ == '__main__':
